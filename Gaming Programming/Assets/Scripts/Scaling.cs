6991ba340caa6119098a5b48328d6c754288b38a
Celeste <BrandonKool16@gmail.com>
2018-09-10 13:52:02 -0400

    [ADD]
    Vector Rotate and Position
    
    By Celeste

---
+++ Gaming Programming/Assets/Scripts/Scaling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaling : MonoBehaviour {
+    public GameObject scale;
+    public float point1, point2;
+    public bool Direct; // Right
+    public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Direct)
+        {
+            scale.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else
+        {
+            scale.transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (scale.transform.position.x >= point1)
+        {
+            Direct = false;
+        }
+        if (scale.transform.position.x <= point2)
+        {
+            Direct = true;
+        }
+    }
+}
---
+++ Gaming Programming/Assets/Scripts/Vectors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vectors : MonoBehaviour {
+
+    public GameObject Ball;
+    public float x, y, z;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        x = Random.Range(2, 10);
+        y = Random.Range(2, 10);
+        z = Random.Range(2, 10);
+        Ball.transform.Rotate(x, y, z);
+	}
+}
