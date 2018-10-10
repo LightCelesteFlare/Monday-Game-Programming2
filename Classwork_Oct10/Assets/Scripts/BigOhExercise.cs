ce7e912f8376fc7737e60eab3ba9d6b1199e2497
Celeste <BrandonKool16@gmail.com>
2018-10-10 10:30:26 -0400

    [Add]

---
+++ Classwork_Oct10/Assets/Scripts/BigOhExercise.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BigOhExercise : MonoBehaviour
+{
+    public float NumberOfElements = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        //int[] numbers = new int[NumberOfElements];
+        DateTime t1 = DateTime.Now;
+        ArrayList al = new ArrayList();
+        for (int i = 0; 1 < NumberOfElements; i++)
+        {
+            al.Add(UnityEngine.Random.Range(0, NumberOfElements));
+        }
+        DateTime t2 = DateTime.Now;
+
+        al.Sort();
+        DateTime t3 = DateTime.Now; ;
+        Debug.Log("t:" + t1.Millisecond);
+        Debug.Log("t:" + t2.Millisecond);
+        Debug.Log("t:" + t3.Millisecond);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
