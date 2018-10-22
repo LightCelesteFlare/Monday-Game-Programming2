ce24526139372b4f3c74905dd6f4c07b431c81a2
Brandon <Brandonkool16@gmail.com>
2018-10-22 17:03:29 -0400

    [Add]
    Added by Celeste and Friends

---
+++ Unity 2.8/MovementTest/Assets/DucklingAI.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DucklingAI : MonoBehaviour {
+
+    public GameObject Survivor;
+    // Rotation speed
+    public float Rotspeed = 100f;
+    // Bool for the wandering
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+    // movement
+    public float speed = 1f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Wandering();
+    }
+    void Wandering()
+    {
+        if (isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+        if (isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * Rotspeed);
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -Rotspeed);
+        }
+        if (isWalking == true)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+    }
+
+    // 0 - nothing | 1 - right | 2 - left
+    IEnumerator Wander()
+    {
+        // TRGameDev
+        int rotTime = Random.Range(1, 3);       // amount of time it rotates
+        int rotateWait = Random.Range(1, 3);    // wait for the rotate cycle
+        int rotateLorR = Random.Range(0, 3);    // decide if it want to do right or left (bool)
+        int walkWait = Random.Range(1, 3);      // walk wait - is the time between walking
+        int walkTime = Random.Range(1, 5);      // will be walking
+
+        isWandering = true;
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if (rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+
+}
---
+++ Unity 2.8/MovementTest/Assets/Follow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject egg;
+    public float TargetDistance;
+    public float AllowedDistance = 5;
+    public float FollowSpeed;
+    public RaycastHit shot;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        //FollowSpeed = 0f;
+
+
+        //transform.LookAt(player.transform);
+        //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+        //{
+        //    TargetDistance = shot.distance;
+        //    if (TargetDistance >= AllowedDistance)
+        //    {
+        //        FollowSpeed = 0.02f;
+        //        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, FollowSpeed);
+        //    }
+        //    else
+        //    {
+        //        FollowSpeed = 0f;
+        //    }
+        //}
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.tag == "Player")
+        {
+
+            transform.LookAt(player.transform);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+            {
+                TargetDistance = shot.distance;
+                if (TargetDistance >= AllowedDistance)
+                {
+                    FollowSpeed = 0.5f;
+                    transform.position = Vector3.MoveTowards(transform.position, player.transform.position, FollowSpeed);
+                }
+                else
+                {
+                    FollowSpeed = 0f;
+                }
+            }
+        }
+    }
+
+}
