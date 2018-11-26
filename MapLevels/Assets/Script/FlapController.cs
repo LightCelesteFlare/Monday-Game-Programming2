b504d6f2e91d7a987d500bc89a618066cc7b5c10
Celeste <BrandonKool16@gmail.com>
2018-11-26 14:27:42 -0500

    [Add]
    Boids
    
    Boi By Celeste

---
+++ MapLevels/Assets/Script/FlapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlapController : MonoBehaviour {
+    public float SXmin = 1.5f, SXmax = 2.5f;
+    float dx;
+
+    public float flapperduration = 6;
+	// Use this for initialization
+	void Start () {
+        dx = SXmax - SXmin;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.localScale = new Vector3(SXmin + dx / 2 + dx * Mathf.Sin(Time.fixedTime * flapperduration),0.1f,1.1f);
+	}
+}
